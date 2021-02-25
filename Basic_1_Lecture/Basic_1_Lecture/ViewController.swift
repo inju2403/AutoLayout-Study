eaccaa2a5feeeeccb39422dd149b0d9c81919c0d
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-02-26 00:30:41 +0900

    Concept of AutoLayout Anchor

--- Basic_1_Lecture/Basic_1_Lecture/ViewController.swift
+++ Basic_1_Lecture/Basic_1_Lecture/ViewController.swift
@@ -12,6 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myButton = UIButton.init(type: .system)
+        myButton.setTitle("My Button", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 100).isActive = true
+        
+        
+        let myLabel = UILabel.init()
+        myLabel.text = "My Label"
+        
+        self.view.addSubview(myLabel)
+        
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        myLabel.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 50).isActive = true
+        myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        
     }
 
 
