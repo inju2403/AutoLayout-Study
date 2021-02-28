8981d335f45c3a6bebcfe1dcc825cd79ea59f2b1
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-02-28 20:25:44 +0900

    Positioning buttons and Sets the angle of the buttons

--- Calculator_StackView/Calculator_StackView/ViewController.swift
+++ Calculator_StackView/Calculator_StackView/ViewController.swift
@@ -11,6 +11,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn10: UIButton!
+    @IBOutlet weak var btn11: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +28,22 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         resultLabel.layer.cornerRadius = 20
-        resultLabel.layer.masksToBounds = true // cornerRaidus  변경을 가능하게 함
+////        resultLabel.layer.masksToBounds = true // cornerRaidus  변경을 가능하게 함
         resultLabel.clipsToBounds = true //위의 코드와 동일한 기능
         
-        btn1.layer.cornerRadius = 20
-        btn1.clipsToBounds = true
+        btn1.layer.cornerRadius = btn1.bounds.width/2
+        btn2.layer.cornerRadius = btn1.bounds.width/2
+        btn3.layer.cornerRadius = btn1.bounds.width/2
+        btn4.layer.cornerRadius = btn1.bounds.width/2
+        btn5.layer.cornerRadius = btn1.bounds.width/2
+        btn6.layer.cornerRadius = btn1.bounds.width/2
+        btn7.layer.cornerRadius = btn1.bounds.width/2
+        btn8.layer.cornerRadius = btn1.bounds.width/2
+        btn9.layer.cornerRadius = btn1.bounds.width/2
+        btn10.layer.cornerRadius = btn1.bounds.height/2
+        btn11.layer.cornerRadius = btn1.bounds.width/2
     }
 
 
