22151665efa468827e72dfc1a2a228208e44a655
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-02-28 18:02:07 +0900

    change cornerRadius

--- Calculator_StackView/Calculator_StackView/ViewController.swift
+++ Calculator_StackView/Calculator_StackView/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var btn1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        resultLabel.layer.cornerRadius = 20
+        resultLabel.layer.masksToBounds = true // cornerRaidus  변경을 가능하게 함
+        resultLabel.clipsToBounds = true //위의 코드와 동일한 기능
+        
+        btn1.layer.cornerRadius = 20
+        btn1.clipsToBounds = true
     }
 
 
