33223a533ff4a6ee4975f961aff46864383c0649
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-01 11:09:16 +0900

    add CustomKeyboard.swift and CustomKeyboard.xib

---
+++ Custom Keyboard/Custom Keyboard/CustomKeyboard.swift
@@ -0,0 +1,20 @@
+//
+//  CustomKeyboard.swift
+//  Custom Keyboard
+//
+//  Created by 이승주 on 2021/03/01.
+//
+
+import UIKit
+
+class CustomKeyboard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
