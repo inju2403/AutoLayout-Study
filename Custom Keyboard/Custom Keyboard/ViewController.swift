6edfebfb63bd533fb8612dfa11b819095838ff24
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-01 10:45:35 +0900

    Custom Keyboard initial set up

---
+++ Custom Keyboard/Custom Keyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Keyboard
+//
+//  Created by 이승주 on 2021/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
