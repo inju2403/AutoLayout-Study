3dccadb4c8baa65e2189d2d3996434bbbff3ef44
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-03 00:01:19 +0900

    troubleshooting how the row and the height is set to automatic when set to AutoLayout

--- Expand Cell/Expand Cell/ViewController.swift
+++ Expand Cell/Expand Cell/ViewController.swift
@@ -28,6 +28,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let textArray = ["short Text",
                          "long long long long long long long long long long long long long long long long long long long long Text",
                          "short Text",
+                         "long long long long long long long long long long long long long long long long long long long long Text",
+                         "short Text",
+                         "long long long long long long long long long long long long long long long long long long long long Text",
+                         "short Text",
                          "long long long long long long long long long long long long long long long long long long long long Text"]
         
         for(_, value) in textArray.enumerated() {
@@ -53,9 +57,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return
+//    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand
         tableView.reloadRows(at: [indexPath], with: .none)
+        
+        // 오토레이아웃으로 설정 했을 경우 row height가 오토메틱으로 설정되는 부분들이 조금은 꼬일 수 있음 -> 이런저런 시도를 해봐야 한다
+        
+        // 1. heightForRowAt에서 정확한 높이를 지정 -> 확실히 해결
+        
+        // 2. tableView.reloadData() -> 어느정도 해결
+        
+        // 3. 어느정도 해결
+//        tableView.estimatedSectionHeaderHeight = 0
+//        tableView.estimatedSectionFooterHeight = 0
+        
+        // 4. 애니메이션 효과 없애기 -> 확실히 해결
+        
+//        UIView.setAnimationsEnabled(false)
+//        tableView.reloadRows(at: [indexPath], with: .none)
+//        UIView.setAnimationsEnabled(true) // false에서 reload하고 다시 true로 바꿔준다(모든 애니메이션이 계속 중지되면 안되기 때문)
     }
     
 }
