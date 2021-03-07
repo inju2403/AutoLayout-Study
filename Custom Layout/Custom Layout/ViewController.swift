716a5fdd36db3492bd3310b0c5dd11428740f6b7
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-08 00:41:17 +0900

    Custom Layout initial set up

---
+++ Custom Layout/Custom Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Layout
+//
+//  Created by 이승주 on 2021/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
