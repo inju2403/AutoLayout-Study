913e39e37d34e1d6456e44f65476965af8d06ffd
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-07 15:55:31 +0900

    add animation to FloatingButtonListViewController

--- Floating Button/Floating Button/FloatingButtonListViewController.swift
+++ Floating Button/Floating Button/FloatingButtonListViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class FloatingButtonListViewController: UIViewController {
 
+    @IBOutlet weak var btn1CenterY: NSLayoutConstraint!
+    @IBOutlet weak var btn2CenterY: NSLayoutConstraint!
+    @IBOutlet weak var btn3CenterY: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        btn1CenterY.constant = 0
+        btn2CenterY.constant = 0
+        btn3CenterY.constant = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 1) {
+            self.btn1CenterY.constant = 80
+            self.btn2CenterY.constant = 160
+            self.btn3CenterY.constant = 240
+            
+            self.view.layoutIfNeeded() // 화면 갱신
+        }
+    
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
