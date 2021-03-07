2667aa128bf15fc29dd3504165cff1c15d04ce50
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-07 23:19:40 +0900

    use extension to change Layout Constraint

--- Graph/Graph/ViewController.swift
+++ Graph/Graph/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
+extension NSLayoutConstraint {
+    func changeMultiplier(value: CGFloat) -> NSLayoutConstraint {
+        
+        NSLayoutConstraint.deactivate([self])
+        
+        let newConstraint = NSLayoutConstraint.init(item: self.firstItem, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: value, constant: self.constant)
+        
+        newConstraint.priority = self.priority
+        newConstraint.shouldBeArchived = self.shouldBeArchived
+        newConstraint.identifier = self.identifier
+        
+        NSLayoutConstraint.activate([newConstraint])
+        
+        return newConstraint
+        
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var graph1Height: NSLayoutConstraint!
+    @IBOutlet weak var graph2Height: NSLayoutConstraint!
+    @IBOutlet weak var graph3Height: NSLayoutConstraint!
+    @IBOutlet weak var graph4Height: NSLayoutConstraint!
+    @IBOutlet weak var graph5Height: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        graph1Height = graph1Height.changeMultiplier(value: 0.9)
+        graph1Height = graph1Height.changeMultiplier(value: 0.8)
+        graph1Height = graph1Height.changeMultiplier(value: 0.7)
+        graph1Height = graph1Height.changeMultiplier(value: 0.6)
+        graph1Height = graph1Height.changeMultiplier(value: 0.9)
+        
     }
     
 }
