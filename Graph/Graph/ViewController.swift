9abb513015e247b3aa2609313a6bb89480b6cf5c
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-08 00:32:19 +0900

    add button action to ViewController

--- Graph/Graph/ViewController.swift
+++ Graph/Graph/ViewController.swift
@@ -37,12 +37,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         graph1Height = graph1Height.changeMultiplier(value: 0.9)
-        graph1Height = graph1Height.changeMultiplier(value: 0.8)
-        graph1Height = graph1Height.changeMultiplier(value: 0.7)
-        graph1Height = graph1Height.changeMultiplier(value: 0.6)
-        graph1Height = graph1Height.changeMultiplier(value: 0.9)
+        graph2Height = graph2Height.changeMultiplier(value: 0.8)
+        graph3Height = graph3Height.changeMultiplier(value: 0.7)
+        graph4Height = graph4Height.changeMultiplier(value: 0.6)
+        graph5Height = graph5Height.changeMultiplier(value: 0.9)
         
     }
     
+    @IBAction func style1(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.5)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.6)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.7)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.8)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.9)
+            
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func style2(_ sender: UIButton) {
+        graph1Height = graph1Height.changeMultiplier(value: 0.9)
+        graph2Height = graph2Height.changeMultiplier(value: 0.8)
+        graph3Height = graph3Height.changeMultiplier(value: 0.7)
+        graph4Height = graph4Height.changeMultiplier(value: 0.6)
+        graph5Height = graph5Height.changeMultiplier(value: 0.5)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
 
