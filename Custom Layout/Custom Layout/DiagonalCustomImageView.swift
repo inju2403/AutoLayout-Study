da84687e1d9267a7ce4f7d5de6d086c4af928ca2
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-09 10:04:52 +0900

    add a variable that are available on the storyboard

--- Custom Layout/Custom Layout/DiagonalCustomImageView.swift
+++ Custom Layout/Custom Layout/DiagonalCustomImageView.swift
@@ -18,12 +18,14 @@ class DiagonalCustomImageView: UIImageView {
     
     // layer -> mask
     
+    @IBInspectable var innerHeight: CGFloat = 0
+    
     func makePath() -> UIBezierPath {
         let path = UIBezierPath()
         path.move(to: CGPoint.zero)
         path.addLine(to: CGPoint.init(x: self.bounds.width, y: 0))
         path.addLine(to: CGPoint.init(x: self.bounds.width, y: self.bounds.height))
-        path.addLine(to: CGPoint.init(x: 0, y: self.bounds.height - 100))
+        path.addLine(to: CGPoint.init(x: 0, y: self.bounds.height - innerHeight))
         path.close() // (0,0)으로 그리는 코드와 같은 역할을 함
         
         return path
