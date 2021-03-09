f918ac26070564d6776e638b27e61f3d06d5902c
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-09 22:26:03 +0900

    connect IBOutlet to ViewController

--- Custom Switch/Custom Switch/ViewController.swift
+++ Custom Switch/Custom Switch/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swithBtn: UIButton!
+    @IBOutlet weak var switchBG: UIView!
+    @IBOutlet weak var buttonCenterX: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
