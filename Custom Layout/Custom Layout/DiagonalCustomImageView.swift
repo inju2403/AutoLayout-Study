57dad07a3ba0921487098e087e2cdaf05765bbf6
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-09 09:17:55 +0900

    DiagonalCustomImageView.swift

---
+++ Custom Layout/Custom Layout/DiagonalCustomImageView.swift
@@ -0,0 +1,20 @@
+//
+//  DiagonalCustomImageView.swift
+//  Custom Layout
+//
+//  Created by 이승주 on 2021/03/09.
+//
+
+import UIKit
+
+class DiagonalCustomImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
