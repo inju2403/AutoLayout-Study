cc2ce34ec1c21986a62e780aba9c048bb1943d27
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-13 23:05:08 +0900

    add a function to close popup

--- Popup/Popup/PopupViewController.swift
+++ Popup/Popup/PopupViewController.swift
@@ -15,15 +15,8 @@ class PopupViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closePopup(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
     }
-    */
-
+    
 }
