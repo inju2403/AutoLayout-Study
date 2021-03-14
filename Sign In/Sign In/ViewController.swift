81d0e8654f156b468f7251309f3ccae444de7708
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-14 21:52:40 +0900

    add isValidEmail function

--- Sign In/Sign In/ViewController.swift
+++ Sign In/Sign In/ViewController.swift
@@ -33,6 +33,16 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    func isValidEmail(email:String?) -> Bool {
+        
+        guard email != nil else { return false }
+        
+        let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let pred = NSPredicate(format:"SELF MATCHES %@", regEx)
+        return pred.evaluate(with: email)
+    }
 
 
 }
