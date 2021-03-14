f9c0e10ef5fc047a48f55ac2d1149a491269d3e7
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-14 21:22:57 +0900

    connect views to ViewController and add addTarget method to textfields

--- Sign In/Sign In/ViewController.swift
+++ Sign In/Sign In/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailError: UILabel!
+    @IBOutlet weak var passwordError: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //textfield 값이 변경되는 걸 캐치하도록 세팅
+        emailTextField.addTarget(self, action: #selector(textFieldEdited), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldEdited), for: .editingChanged)
+    }
+    
+    @objc func textFieldEdited(textField: UITextField) {
+        
+        if textField == emailTextField {
+            
+        } else if textField == passwordTextField {
+            
+        }
+        
     }
 
 
