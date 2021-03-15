7769fa03c142ba49b4c140f6ec513a8314d94d21
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-15 17:48:45 +0900

    add tableViewCell

---
+++ Chat App/Chat App/MyCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCell.swift
+//  Chat App
+//
+//  Created by 이승주 on 2021/03/15.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
