e0196962a66bfd0d5f0b9f34e8e8438fc57e9164
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-15 20:42:11 +0900

    connect IBOutlets, IBAction and register Cell to TableView

--- Chat App/Chat App/ViewController.swift
+++ Chat App/Chat App/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        chatTableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "myCell")
+        chatTableView.register(UINib(nibName: "YourCell", bundle: nil), forCellReuseIdentifier: "yourCell")
     }
 
-
+    @IBAction func sendString(_ sender: UIButton) {
+    }
+    
 }
 
